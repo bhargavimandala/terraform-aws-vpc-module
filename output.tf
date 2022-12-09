6831c6c9b9a7acb97bcf6671b2f532ee323e305d
bhargavimandala <51246470+bhargavimandala@users.noreply.github.com>
2022-12-09 10:11:20 +0000

    Added output file for subnets

--- output.tf
+++ output.tf
@@ -1,4 +1,14 @@
 output "vpc_id" {
   value = aws_vpc.vpc.id
+  description = "The ID of the VPC"
 }
 
+output "public_subnet" {
+ value = aws_subnet.public_subnets.*.id
+ description = "The ID of the public subnet"
+}
+
+output "private_subnet" {
+ value = aws_subnet.private_subnets.*.id
+ description = "The ID of the private subnet"
+}
